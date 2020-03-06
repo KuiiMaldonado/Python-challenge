60ee9b6a1a41fa173bd35c9481fd23570c15f660
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2020-03-05 18:06:25 -0600

    PyBank script done

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,23 +1,24 @@
-import os
+#Importing modules
 import os
 import csv
 
-#Initializing months counter
+#Initializing variables
 months = 0
 totalAmount = 0
 maxIncrease = 0
 maxDecrease = 0
+prev = 0
+actual = 0
+change = 0
 
 #Get the path where file is located
 inputFilePath = os.path.join("..", "Resources", "budget_data.csv")
 
-#Get the path for output file will be located
+#Get the path where output file will be located
 outputFilePath = os.path.join("..", "Resources", "output_budget_data.txt")
 
-print(inputFilePath)
-
 #Opening file
-with open(inputFilePath, 'r') as csvFile:
+with open(inputFilePath, 'r', encoding="UTF-8") as csvFile:
     csvReader = csv.reader(csvFile, delimiter=",")
 
     #Headers row
@@ -25,28 +26,55 @@ with open(inputFilePath, 'r') as csvFile:
 
     #Loop through all the rows to get all data needed
     for row in csvReader:
+
+        #Adding total amount for entire period
         totalAmount = totalAmount + int(row[1])
+        
+        #Get actual amount
+        actual = int(row[1])
+
+        #Calculate change. We skip first row because previous change is zero
+        if prev != 0:
+            change += (actual - prev)
+
+        #Looking for max Increase and max Decrease
         if int(row[1]) > maxIncrease:
             maxIncrease = int(row[1])
             increaseDate = row[0]
         elif int(row[1]) < maxDecrease:
             maxDecrease = int(row[1])
             decreaseDate = row[0]
+
+        #Getting previous amount
+        prev =int(row[1])
+
+        #Adding for total months in period
         months += 1
 
-txtWriter = open(outputFilePath, "w")
+#Get avergae of changes
+change = change/(months - 1)
+
+#Opening output file in write mode
+txtWriter = open(outputFilePath, "w", encoding="UTF-8")
+
+#Writing data to txt file
 txtWriter.write("Financial Analysis\n")
 txtWriter.write("-----------------------------\n")
 txtWriter.write(f'Total Months: {months}\n')
-txtWriter.write(f'Average Change: \n')
+txtWriter.write(f'Total: ${totalAmount}\n')
+txtWriter.write(f'Average Change: $%.02f\n' %change)
 txtWriter.write(f'Greatest Increase in Profits: {increaseDate} - (${maxIncrease})\n')
 txtWriter.write(f'Greatest Decrease in Profits: {decreaseDate} - (${maxDecrease})\n')
+
+#Closing the file
 txtWriter.close()
 
-
-print(f'total amount: {totalAmount}')
-print(f'months: {maxIncrease}')
-print(f'months: {maxDecrease}')
-print(f'months: {months}')
-
+#Printing out to terminal
+print("Financial Analysis")
+print("-----------------------------")
+print(f'Total Months: {months}')
+print(f'Total: ${totalAmount}')
+print(f'Average Change: $%.02f' %change)
+print(f'Greatest Increase in Profits: {increaseDate} - (${maxIncrease})')
+print(f'Greatest Decrease in Profits: {decreaseDate} - (${maxDecrease})')
     
